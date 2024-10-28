22538d6c97ed76b13950f12d9a137a6f22830dde
Aiden Nguyen <lamaiden124@gmail.com>
2024-10-28 19:18:50 +1030

    feat: implemented the first fetch request to populate a dropdown for the camera suburbs

--- Assig2/a2reactapp/src/Components/Dashboard.js
+++ Assig2/a2reactapp/src/Components/Dashboard.js
@@ -16,7 +16,7 @@ function Dashboard() {
         fetch(`http://localhost:5147/api/Get_ListCameraSuburbs`)
             .then(response => response.json())
             .then(data => {
-                console.log(data)
+                setCameraSuburbs(data);
             })
     }, [])
 
@@ -30,7 +30,12 @@ function Dashboard() {
 
                   <div className="dropdown-section">
                       <select className="dropdown">
-                          <option value="option1">Dropdown 1</option>
+                          <option value="">Select a suburb</option>
+                          {cameraSuburbs.map(cameraSuburbs => (
+                              <option key={cameraSuburbs} value={cameraSuburbs}>
+                                  {cameraSuburbs}
+                              </option>
+                          )) }
                       </select>
                       <select className="dropdown">
                           <option value="option1">Dropdown 2</option>
