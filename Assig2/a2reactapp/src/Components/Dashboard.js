84791b6146ed2c19e44fd5c19b277e1aeb2bb90d
Aiden Nguyen <lamaiden124@gmail.com>
2024-10-28 18:29:18 +1030

    WIP: dashboard contents styling

--- Assig2/a2reactapp/src/Components/Dashboard.js
+++ Assig2/a2reactapp/src/Components/Dashboard.js
@@ -9,7 +9,29 @@ function Dashboard() {
         <Navbar />
 
           <div className="dashboard-content">
+              <div className="rectangle">
+                  <h3 className="dashboard-title">Dashboard Title</h3>
 
+                  <div className="dropdown-section">
+                      <select className="dropdown">
+                          <option value="option1">Option 1</option>
+                      </select>
+                      <select className="dropdown">
+                          <option value="option1">Option 1</option>
+                      </select>
+                      <select className="dropdown">
+                          <option value="option1">Option 1</option>
+                      </select>
+                  </div>
+
+                  <div className="scrollable-list">
+                      <ul>
+                          {Array.from({ length: 8 }, (_, index) => (
+                              <li key={index}>List Item {index + 1}</li>
+                          ))}
+                      </ul>
+                  </div>
+              </div>
           </div>
     </div>
   );
