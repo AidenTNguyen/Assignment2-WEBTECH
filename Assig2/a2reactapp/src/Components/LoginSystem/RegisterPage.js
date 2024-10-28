196af4dbf4b3b9c09738a0a9c79893fd43956ffc
Aiden Nguyen <lamaiden124@gmail.com>
2024-10-28 14:58:53 +1030

    WIP: will dynamically check if the confirmation password field matches the password field

--- Assig2/a2reactapp/src/Components/LoginSystem/RegisterPage.js
+++ Assig2/a2reactapp/src/Components/LoginSystem/RegisterPage.js
@@ -4,13 +4,17 @@ import coatOfArms from '../coatOfArms.png';
 import './LoginPageStyle.css'
 
 function RegisterPage() {
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [error, setError] = useState('');
+
     return (
       // Reusing the login style so i dont have to create new ones just to replicate the current login page ones
       <div className="LoginPage">
           <div className="login-container">
               <form>
                   <img src={coatOfArms} alt="SAPOL coat of arms" />
-                  <h3>Login</h3>
+                  <h3>Register</h3>
 
                   <label for="username">Username</label>
                   <input type="text" placeholder="Username" id="username" />
@@ -18,8 +22,11 @@ function RegisterPage() {
                   <label for="username">Password</label>
                   <input type="password" placeholder="Password" id="password" />
 
+                  <label for="username">Confirm Password</label>
+                  <input type="password" placeholder="Confirm Password" id="passwordConfirmation" />
+
                   <hr />
-                  <button>Log In</button>
+                  <button>Register</button>
               </form>
           </div>
       </div>
