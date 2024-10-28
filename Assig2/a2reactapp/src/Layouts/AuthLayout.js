ca7eebe47158adc7b75fccb3c14327c51b1db4e2
Aiden Nguyen <lamaiden124@gmail.com>
2024-10-28 17:12:54 +1030

    WIP: separating the style of the authentication pages from the main pages

---
+++ Assig2/a2reactapp/src/Layouts/AuthLayout.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Components/LoginSystem/LoginPage';
+import RegisterPage from './Components/LoginSystem/RegisterPage';
+import Dashboard from './Components/Dashboard';
+import LocationReport from './Components/LocationReport';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Router>
+            <Routes>
+                <Route path="/" element={<App />}>
+                    <Route path="/LoginPage" element={<LoginPage />} />
+                    <Route path="/RegisterPage" element={<RegisterPage />} />
+                    <Route path="/Dashboard" element={<Dashboard />} />
+                    <Route path="/LocationReport" element={<LocationReport />} />
+                    <Route path="" element={<LoginPage />} />
+                    <Route path="*" element={<LoginPage />} />
+                </Route>
+            </Routes>
+        </Router>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
