00f6ae09293a796ccf642690943c0a3c44f7c8b4
Aiden Nguyen <lamaiden124@gmail.com>
2024-11-13 09:41:34 +1030

    feat: implemented the 4th filter for expiation descriptions

--- Assig2/a2reactapp/src/Components/DashboardFilters/ExpiationDescFilter.js
+++ Assig2/a2reactapp/src/Components/DashboardFilters/ExpiationDescFilter.js
@@ -1,35 +1,43 @@
 import React, { useEffect, useState } from 'react';
-function ExpiationDescFilter({ expiationDescriptionChangeFunction }) {
 
-    const [input, setInput] = useState('');
+function ExpiationDescFilter({ expiationDescriptionChangeFunction }) {
+    const [input, setInputValue] = useState('');
     const [suggestions, setSuggestions] = useState([]);
-    const [filteredSuggestions, setFilteredSuggestions] = useState([])
 
     useEffect(() => {
         fetch(`http://localhost:5147/api/Get_SearchOffencesByDescription?offenceCodesOnly=false`)
             .then(response => response.json())
             .then(data => {
-                setSuggestions(data)
-                console.log(data)
-            })
-    }, [])
+                setSuggestions(data.map(item => item.description));
+            });
+    }, []);
 
-    const handleExpiationDescriptionSelection = (event) => {
-        let value = event.target.value;
-        setInput(value);
+    const handleInputChange = (event) => {
+        const value = event.target.value;
+        setInputValue(value);
         expiationDescriptionChangeFunction(value);
 
-    }
+        if (event.target.value === '') {
+            expiationDescriptionChangeFunction("noSelection")
+        }
+    };
 
-    return ( 
+    return (
         <div className="autocomplete">
             <input
                 type="text"
                 placeholder="Expiation Description..."
-                value={""}
-                onChange={handleExpiationDescriptionSelection}
+                value={input}
+                onChange={handleInputChange}
+                list="expiation-suggestions"
                 style={{ padding: "10px", borderRadius: "5px", border: "1px solid #ccc", width: "100%" }}
             />
+
+            <datalist id="expiation-suggestions">
+                {suggestions.map((suggestion, index) => (
+                    <option key={index} value={suggestion} />
+                ))}
+            </datalist>
         </div>
     );
 
